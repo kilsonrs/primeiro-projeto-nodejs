c12bb6f68b00fca00cc184c9ec9983ddacb9d3a5
Kilson <kilsonrs@gmail.com>
2020-08-17 08:42:06 -0300

    Alternative solution

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -24,24 +24,45 @@ class TransactionsRepository {
   }
 
   public getBalance(): Balance {
-    const income = this.transactions.reduce((total, transaction) => {
-      if (transaction.type === 'income') {
-        return total + transaction.value;
-      }
-      return total;
-    }, 0);
-    const outcome = this.transactions.reduce((total, transaction) => {
-      if (transaction.type === 'outcome') {
-        return total + transaction.value;
-      }
-      return total;
-    }, 0);
-    const balance = {
-      income,
-      outcome,
-      total: income - outcome,
-    };
-    return balance;
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    // const income = this.transactions.reduce((total, transaction) => {
+    //   if (transaction.type === 'income') {
+    //     return total + transaction.value;
+    //   }
+    //   return total;
+    // }, 0);
+    // const outcome = this.transactions.reduce((total, transaction) => {
+    //   if (transaction.type === 'outcome') {
+    //     return total + transaction.value;
+    //   }
+    //   return total;
+    // }, 0);
+    // const balance = {
+    //   income,
+    //   outcome,
+    //   total: income - outcome,
+    // };
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 
   public create({ title, value, type }: CreateTransactionDTO): Transaction {
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,6 +15,9 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transaction type is invalid');
+    }
     const balance = this.transactionsRepository.getBalance();
     if (type === 'outcome' && balance.total < value) {
       throw new Error("You don't have enough balance.");
